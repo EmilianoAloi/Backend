cab878a7a116b1cadfdc560cdbc39dadd2d27a35
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2023-11-03 13:33:35 -0300

    Se agrego ruta para obtener productos de un cart by ID

--- src/managers/CartManager.js
+++ src/managers/CartManager.js
@@ -2,8 +2,31 @@ import fs from 'fs';
 
 export class CartManager {
     constructor() {
-        this.path = './cart.json';
+        this.path = './src/data/carts.json';
     }
+
+
+
+    async getCartsById(cid) {
+        try {
+            if (fs.existsSync(this.path)) {
+                const cartsJSON = await fs.promises.readFile(this.path, "utf-8");
+                const carts = JSON.parse(cartsJSON);
+                const cartById = carts.find((cart) => cart.id === cid)
+                if (cartById) return cartById
+                else return "No se ha encontrado un Cart con ese ID" 
+
+            } else return "No hay ningun Cart"
+
+        } catch (error) {
+            console.log(error);
+
+        }
+
+    }
+
+
+
 
 
 
