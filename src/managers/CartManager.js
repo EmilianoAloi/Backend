aa84d73dee3b8d1fedbde48a32fd3f83a918feac
Emiliano Aloi <emilianoaloi.dev@gmail.com>
2023-11-03 19:37:04 -0300

    se creo ruta post para crear cart

--- src/managers/CartManager.js
+++ src/managers/CartManager.js
@@ -6,6 +6,41 @@ export class CartManager {
     }
 
 
+    async getCarts() {
+        try {
+            if (fs.existsSync(this.path)) {
+                const cartsJSON = await fs.promises.readFile(this.path, "utf-8");
+                const carts = JSON.parse(cartsJSON);
+                return carts;
+            } else return [];
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+
+    async addCart(products) {
+        try {
+            const carts = await this.getCarts();
+
+            const newCart = {
+                id: carts.length + 1,
+                products: products
+            }
+
+            carts.push(newCart);
+
+            await fs.promises.writeFile(this.path, JSON.stringify(carts));
+            return newCart
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+
+
 
     async getCartsById(cid) {
         try {
@@ -14,7 +49,7 @@ export class CartManager {
                 const carts = JSON.parse(cartsJSON);
                 const cartById = carts.find((cart) => cart.id === cid)
                 if (cartById) return cartById
-                else return "No se ha encontrado un Cart con ese ID" 
+                else return "No se ha encontrado un Cart con ese ID"
 
             } else return "No hay ningun Cart"
 
